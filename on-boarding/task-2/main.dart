fc594b6d99fb9e7ad559fb07e7e484548451b258
abi26anamo <abianamo282@gmail.com>
2023-08-01 17:52:53 +0300

    updated main.dart

--- on-boarding/task-2/main.dart
+++ on-boarding/task-2/main.dart
@@ -4,24 +4,30 @@ import 'task_manager.dart';
 void main() {
   TaskManager taskManager = TaskManager();
   var task1 = Task(
-      title: "Task 1",
-      description: "This is task 1",
-      dueDate: DateTime.now(),
-      taskStatus: status.completed);
+    title: "Task 1",
+    description: "This is task 1",
+    dueDate: DateTime.now(),
+    taskStatus: status.completed,
+  );
   taskManager.addTask(task1);
   var task2 = Task(
-      title: "Task 2",
-      description: "This is task 2",
-      dueDate: DateTime.now(),
-      taskStatus: status.pending);
+    title: "Task 2",
+    description: "This is task 2",
+    dueDate: DateTime.now(),
+    taskStatus: status.pending,
+  );
+  
   taskManager.addTask(task2);
-
   taskManager.viewAllTasks();
   taskManager.viewCompletedTasks();
   taskManager.viewPendingTasks();
 
-  taskManager.editTask(task1, "Task 1 edited", "This is task 1 edited",
-      DateTime.now().add(Duration(days: 1)));
+  taskManager.editTask(
+    task1,
+    "Task 1 edited",
+    "This is task 1 edited",
+    DateTime.now().add(Duration(days: 1)),
+  );
 
   taskManager.deleteTask(task2);
 }

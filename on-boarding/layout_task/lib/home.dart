8e0ab361cb829f8f3afa73f59ac2d119c4aa3575
abi26anamo <abianamo282@gmail.com>
2023-08-05 15:55:06 +0300

    added layout task

---
+++ on-boarding/layout_task/lib/home.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.red,
+          ),
+          Positioned(
+            child: Image.asset(
+              'assets/images/stick-man-canva.png',
+              height: 400,
+              width: 400,
+            ),
+            top: 50,
+            left: 10,
+            right: 10,
+          ),
+          Positioned(
+            bottom: 50,
+            right: 50,
+            child: Container(
+              width: 256,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(12, 140, 233, 1),
+              ),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Get.to(TodoListPage());
+                },
+                child: Text(
+                  "Get Started",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 19,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}

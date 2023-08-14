9a5fff76ba075130cd9895ea6889285eb2cfd3f6
abi26anamo <abianamo282@gmail.com>
2023-08-14 10:48:12 +0300

    completed local datasource task

--- on-boarding/todo/lib/features/todo/data/datasources/task_local_data_source.dart
+++ on-boarding/todo/lib/features/todo/data/datasources/task_local_data_source.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
+
+import 'package:layout_task/core/errors/exception.dart';
 import 'package:layout_task/features/todo/domain/entities/task.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/todo_model.dart';
 
-abstract class TaskLocalDataSource{
+const CACHED_TASK = 'CACHED_TASK';
+
+abstract class TaskLocalDataSource {
   Future<TodoTask> getLastTask();
 
   Future<void> cacheTask(TodoModel taskToCache);
@@ -10,5 +16,43 @@ abstract class TaskLocalDataSource{
   Future<void> cacheAllTask(List<TodoModel> taskToCache);
 
   Future<List<TodoTask>> getAllCachedTasks();
+}
+
+class TaskLocalDataSourceImpl implements TaskLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  TaskLocalDataSourceImpl({required this.sharedPreferences});
+
+  @override
+  Future<TodoTask> getLastTask() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final jsonString = prefs.getString(CACHED_TASK);
+
+    if (jsonString != null) {
+      return Future.value(TodoModel.fromJson(json.decode(jsonString)));
+    } else {
+      throw CacheException();
+    }
+  }
+
+  @override
+  Future<void> cacheTask(TodoModel taskToCache) {
+    return sharedPreferences.setString(
+      CACHED_TASK,
+      json.encode(
+        taskToCache.toJson(),
+      ),
+    );
+  }
+  
+  @override
+  Future<void> cacheAllTask(List<TodoModel> taskToCache) {
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<List<TodoTask>> getAllCachedTasks() {
+    throw UnimplementedError();
+  }
 
 }
---
+++ on-boarding/todo/test/feature/todo/data/datasource/task_local_data_source_test.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:layout_task/core/errors/exception.dart';
+import 'package:layout_task/features/todo/data/datasources/task_local_data_source.dart';
+import 'package:layout_task/features/todo/data/models/todo_model.dart';
+import 'package:mockito/mockito.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../../../../fixture/fixure_reader.dart';
+
+class MockSharedPreferences extends Mock implements SharedPreferences {}
+
+void main() {
+  late MockSharedPreferences mockSharedPreferences;
+  late TaskLocalDataSourceImpl datasource;
+
+  setUp(() {
+    mockSharedPreferences = MockSharedPreferences();
+    datasource =
+        TaskLocalDataSourceImpl(sharedPreferences: mockSharedPreferences);
+  });
+
+  group("get last task", () {
+    final todoModel =
+        TodoModel.fromJson(json.decode(fixture("todo_cached.json")));
+
+    test(
+        " should return task from sharedpreferences when there is one in cache",
+        () async {
+      when(mockSharedPreferences.getString(CACHED_TASK))
+          .thenReturn(fixture("todo_cached.json"));
+
+      final result = await datasource.getLastTask();
+
+      verify(mockSharedPreferences.getString(CACHED_TASK));
+      expect(result, equals(todoModel));
+    });
+
+    test("should return cacheException when there is no cached task", () {
+      when(mockSharedPreferences.getString(CACHED_TASK)).thenReturn(null);
+      final call = datasource.getLastTask;
+
+      expect(() => call(), throwsA(TypeMatcher<CacheException>));
+    });
+  });
+
+  group("cachetask", () {
+    final todoModel = TodoModel(
+      id: "1",
+      text: "text",
+      title: "title",
+      subtitle: "subtitle",
+      date: "12/423",
+    );
+
+    test("should call sharedprefereces to cache task", () {
+      datasource.cacheTask(todoModel);
+
+      final expectedJsonString = json.encode(todoModel.toJson());
+
+      verify(mockSharedPreferences.setString(CACHED_TASK, expectedJsonString));
+    });
+  });
+}  
